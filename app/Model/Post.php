d30cbf50029605c587cb1560b893eb16f90697fc
Naoto Mawatari <github.mawatari@gmail.com>
2012-10-25 15:24:26 +0900

    Finished lesson 16.

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,5 +1,13 @@
 <?php
 
 class Post extends AppModel {
-
+	public $validate = array(
+		'title' => array(
+			'rule' => 'notEmpty',
+			'message' => '空白とかｗｗｗ'
+		),
+		'body' => array(
+			'rule' => 'notEmpty'
+		)
+	);
 }
